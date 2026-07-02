7a5f881046b2d8286854d228d5486d1f097a6248
Sergey Bashkirov <elevrus9@gmail.com>
2026-07-02 22:24:03 +0500

    debug backend

--- backend/app/api/app.py
+++ backend/app/api/app.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://162.248.164.246:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
